7e329ce1a636755c41eab03cd6c464b9b3e2d7c1
Junho Yoon <45956041+J-hoplin1@users.noreply.github.com>
2022-04-13 15:05:50 +0900

    Update bubble_sort.py

--- Sorting/bubble_sort/python/bubble_sort.py
+++ Sorting/bubble_sort/python/bubble_sort.py
@@ -3,11 +3,9 @@ from typing import MutableSequence
 def bubble_sort(a:MutableSequence,start,end):
     n = start
     for i in range(start,end-1):
-        mi = i
         for j in range(i + 1,end):
-            if a[j] < a[mi]:
-                mi = j
-        a[mi],a[i] = a[i],a[mi]
+            if a[j-1] > a[j]:
+                a[j-1],a[j]= a[j],a[j-1]
 
 a = [3,1,2,5,3,4]
 bubble_sort(a,0,len(a))
